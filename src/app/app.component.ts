9b43f8c6c447cac5a49d03ad8be4312ed9f1a20d
Serge <sty@binotel.com>
2025-12-11 19:43:01 +0100

    add ngrx + material + linter stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSlideToggle} from "@angular/material/slide-toggle";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-boilerplate';
+  protected title = 'angular-boilerplate';
 }
